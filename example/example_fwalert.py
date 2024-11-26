65e0ce92dc24a3f8d24462c347589388eaac4680
Funan Zhou <matrix@blocksec.com>
2024-11-26 22:18:52 +0800

    update: singleton for fwalert

--- example/example_fwalert.py
+++ example/example_fwalert.py
@@ -2,10 +2,8 @@ import asyncio
 import sys
 import os
 
-
 # Add the project root directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 
 from web3_data_center.core.data_center import DataCenter
 from web3_data_center.clients.fwalert_client import FWAlertClient
@@ -14,11 +12,15 @@ import datetime
 import pandas as pd
 from web3 import Web3
 
+async def trigger_alert():
+    # Get the singleton instance
+    fwclient = FWAlertClient.get_instance(config_path="config.yml", use_proxy=False)
+    await fwclient.callme("Vitalik发新推文了")
+
 
 async def main():
-    fwclient = FWAlertClient(config_path="config.yml", use_proxy=False)
-    # await fwclient.callme(f"Vitalik发新推文了")
-    await fwclient.notify("ddd930ea-2719-4c90-94ac-14bfdb59200d", {"topic": "有新的Pump出现", "contract": "0xsadaddsa"})
+    # Run multiple alerts
+    await trigger_alert()
 
 if __name__ == '__main__':
     asyncio.run(main())
